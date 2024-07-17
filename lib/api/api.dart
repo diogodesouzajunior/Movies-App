b4c9422e60e7940ecd3a9b669c9bd9bf16974a20
Diogo de Souza Junior <diogodesouzajr@gmail.com>
2024-07-17 12:29:14 -0300

    Initial commit

---
+++ lib/api/api.dart
@@ -0,0 +1,5 @@
+class Api {
+  static const baseUrl = "https://api.themoviedb.org/3/";
+  static const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
+  static const apiKey = "0e764c30ca1444572ba1379c3a28c107";
+}
